f15188aa4dfe2db0f9705ec7939795239d67b7ab
cppgroup-sea <cppgroup@sea-organic.com>
2025-10-09 14:17:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // !================== PASTE YOUR APPS SCRIPT URL HERE ==================!
-const API_URL = "https://script.google.com/macros/s/AKfycbyE7MPF1Xei1ixIbb_tdOKPOMenFUt_27OqMEtN5SXSMCqF8sH6KPfVxchQU12AjnvE0A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbypzTpL5HNNle6QFt5X8-KpN4ougBX9gDZHqHWUVdl6eSEUKVKM0eJoxRE4ibgc0LkbzA/exec";
 // !=====================================================================!
 
 const loggedInUser = sessionStorage.getItem('stockUser');
